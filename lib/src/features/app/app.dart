c3bd5d29d1ccf1cd3b09b2eaa1669320fdde11fc
mouaz <mouazhariry5@gmail.com>
2025-02-20 17:42:49 +0200

    - init

---
+++ lib/src/features/app/app.dart
@@ -0,0 +1,41 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../localization/current_language.dart';
+import '../../theme/app_theme.dart';
+
+import '../../routing/app_router_provider.dart';
+
+class App extends ConsumerStatefulWidget {
+  const App({super.key});
+
+  @override
+  ConsumerState<App> createState() => _AppState();
+}
+
+class _AppState extends ConsumerState<App> {
+  @override
+  void initState() {
+    Future(() => ref
+        .read(currentLanguageProvider.notifier)
+        .changeLanguage(context, context.locale.languageCode));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final appRouter = ref.watch(appRouterProvider);
+    final currentLanguage = ref.watch(currentLanguageProvider);
+    return MaterialApp.router(
+      routerDelegate: appRouter.delegate(
+          deepLinkBuilder: (deepLink) => DeepLink.defaultPath),
+      routeInformationParser: appRouter.defaultRouteParser(),
+      theme: ref.watch(appThemeProvider),
+      onGenerateTitle: (context) => context.tr('appTitle'),
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: Locale(currentLanguage),
+    );
+  }
+}
