22b67897ca2527dbade42d87e009d4634d81f95a
mouaz <mouazhariry5@gmail.com>
2025-02-20 17:46:59 +0200

    - edit

--- lib/src/shared_widgets/app_empty_data_widget.dart
+++ lib/src/shared_widgets/app_empty_data_widget.dart
@@ -1,6 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:standard_project/src/extenssions/int_extenssion.dart';
 
 import '../../gen/assets.gen.dart';
