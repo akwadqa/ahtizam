2076e724db396c863680d0f56f031c668074d88e
mouaz <mouazhariry5@gmail.com>
2025-02-23 21:18:27 +0200

    - Edit on boarding screens. - Add Cairo fonts style.

--- lib/src/features/onBoarding/presentations/screens/on_boarding_screen.dart
+++ lib/src/features/onBoarding/presentations/screens/on_boarding_screen.dart
@@ -1,10 +1,12 @@
 import 'dart:ui';
 
 import 'package:auto_route/auto_route.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 // import 'package:introduction_screen/introduction_screen.dart';
 import 'package:standard_project/gen/assets.gen.dart';
+import 'package:standard_project/src/extenssions/int_extenssion.dart';
 import 'package:standard_project/src/extenssions/widget_extensions.dart';
 
 import 'package:standard_project/src/shared_widgets/custom_button_widget.dart';
@@ -48,54 +50,18 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                         currentIndex = index;
                       });
                     },
-                    itemCount: onBoardingItems.length,
+                    itemCount: onBoardingItems(context).length,
                     itemBuilder: (context, index) {
-                      return onBoardingItems[index];
+                      return onBoardingItems(context)[index];
                     }),
               ),
-              // Container(
-              //   width: double.infinity,
-              //   height: 50,
-              //   margin: const EdgeInsets.only(
-              //     top: 0,
-              //     bottom: 40,
-              //     left: 20,
-              //     right: 20,
-              //   ),
-              //   child: ElevatedButton(
-              //     style: ElevatedButton.styleFrom(
-              //       backgroundColor: Colors.white,
-              //       shape: RoundedRectangleBorder(
-              //         borderRadius: BorderRadius.circular(15),
-              //       ),
-              //     ),
-              //     onPressed: () {
-              //       if (currentIndex == onBoardingItems.length - 1) {}
-              //       _controller.nextPage(
-              //           duration: Duration(milliseconds: 100),
-              //           curve: Curves.bounceIn);
-              //       // setState(() {
-              //       //   currentIndex++;
-              //       //
-              //       // });
-              //     },
-              //     child: Text(
-              //       currentIndex == onBoardingItems.length - 1
-              //           ? "Get Started"
-              //           : 'Next',
-              //       style: TextStyle(
-              //           fontSize: 20,
-              //           fontWeight: FontWeight.w400,
-              //           color: Colors.black),
-              //     ),
-              //   ),
-              // ),
               CustomButtonWidget(
-                      text: currentIndex == onBoardingItems.length - 1
-                          ? "Get Started"
-                          : 'Next',
+                      text: currentIndex == onBoardingItems(context).length - 1
+                          ? context.tr("get_started")
+                          : context.tr("next"),
                       onTap: () {
-                        if (currentIndex == onBoardingItems.length - 1) {}
+                        if (currentIndex ==
+                            onBoardingItems(context).length - 1) {}
                         _controller.nextPage(
                             duration: Duration(milliseconds: 100),
                             curve: Curves.bounceIn);
@@ -106,16 +72,17 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                       radius: 15,
                       width: MediaQuery.sizeOf(context).width)
                   .onlyPadding(bottom: 20)
+                  .symmetricPadding(horizontal: 15)
             ],
-          ).symmetricPadding(horizontal: 15),
+          ),
           Positioned(
-            bottom: 160,
-            right: 130,
+            bottom: 100,
+            right: 160,
             child: Center(
               child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: List.generate(
-                      onBoardingItems.length, (index) => buildDots(index))),
+                  children: List.generate(onBoardingItems(context).length,
+                      (index) => buildDots(index))),
             ),
           )
         ],
@@ -125,23 +92,76 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
 
   Container buildDots(int index) {
     return Container(
-      height: 5,
-      width: currentIndex == index ? 43 : 32,
+      height: 10,
+      width: currentIndex == index ? 10 : 10,
       margin: EdgeInsets.only(right: 5),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(22),
-        color: currentIndex == index ? AppColors.darkGray : Color(0xFFEC9E03),
+        color: currentIndex == index ? AppColors.primary : AppColors.darkGray,
       ),
     );
   }
 }
 
-final List<Widget> onBoardingItems = [
-  Assets.images.onboarding1Im.image(),
-  Column(
-    children: [
-      Assets.images.onboarding2Im.image(),
-      // Assets.images.onboarding2layerIm.image(),
-    ],
-  ),
-];
+List<Widget> onBoardingItems(BuildContext context) => [
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Assets.images.onboarding1Im.image(),
+          30.verticalSpace,
+          Text(
+            context.tr("welcome_msg"),
+            style:
+                Theme.of(context).textTheme.bodySmall!.copyWith(fontSize: 20),
+          ).symmetricPadding(horizontal: 15),
+          2.verticalSpace,
+          Text(
+            context.tr("welcome_dsc"),
+            style: Theme.of(context)
+                .textTheme
+                .bodySmall!
+                .copyWith(fontSize: 14, color: AppColors.gray),
+          ).symmetricPadding(horizontal: 15),
+          15.verticalSpace,
+        ],
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Stack(
+              children: [
+                Assets.images.onboarding2Im
+                    .image()
+                    .centered()
+                    .onlyPadding(top: 100),
+                Container(),
+                // Assets.images.onboarding2layerIm.image(),
+                Positioned(
+                    bottom: 10,
+                    // bottom: 100,
+                    // right: 160,
+                    child: Assets.images.onboarding2layerIm.image()),
+              ],
+            ),
+          ),
+          60.verticalSpace,
+          Text(
+            context.tr("seconde_welcome_msg"),
+            style:
+                Theme.of(context).textTheme.bodySmall!.copyWith(fontSize: 20),
+          ).symmetricPadding(horizontal: 15),
+          2.verticalSpace,
+          Text(
+            context.tr("seconde_welcome_dsc"),
+            style: Theme.of(context)
+                .textTheme
+                .bodySmall!
+                .copyWith(fontSize: 14, color: AppColors.gray),
+          ).symmetricPadding(horizontal: 15),
+          // 15.verticalSpace,
+        ],
+      ),
+    ];
