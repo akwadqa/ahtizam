3ae4ad1986b3df4f38533bd3dab966beae8e9833
SaraAkwad <sarak@akwad.qa>
2025-02-24 11:59:21 +0300

    Fixes & iOS building.

--- lib/src/features/onBoarding/presentations/screens/on_boarding_screen.dart
+++ lib/src/features/onBoarding/presentations/screens/on_boarding_screen.dart
@@ -1,9 +1,7 @@
-import 'dart:ui';
 
 import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 // import 'package:introduction_screen/introduction_screen.dart';
 import 'package:standard_project/gen/assets.gen.dart';
 import 'package:standard_project/src/extenssions/int_extenssion.dart';
--- lib/src/shared_widgets/app_logo.dart
+++ lib/src/shared_widgets/app_logo.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../../gen/assets.gen.dart';
 import '../theme/app_colors.dart';
 
 class AppLogo extends StatelessWidget {
